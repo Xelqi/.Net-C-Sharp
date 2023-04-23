d93372d3cc9d627375a741d64e9985b1383e668b
Milo <milek1093@gmail.com>
2023-04-23 19:57:08 +0200

    excercise 2 finished

--- Excerice 2/Program.cs
+++ Excerice 2/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Excerice_2
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            string url;
+            string check = "htpps://";
+            Console.WriteLine("Enter a url to see contents:");
+            url = Console.ReadLine();
+
+            if (string.Compare(check, url.Substring(0, 8)) != 0)
+            {
+                url = "https://" + url;
+            }
+            WebRequest wb = WebRequest.Create(url);
+            WebResponse rp = wb.GetResponse();
+            StreamReader reader = new StreamReader(rp.GetResponseStream(), Encoding.ASCII);
+            Console.WriteLine(reader.ReadToEnd());
         }
     }
 }

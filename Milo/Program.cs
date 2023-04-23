888bc6460e5c197bc2022515652e18a61e233145
Milo <milek1093@gmail.com>
2023-04-23 19:19:15 +0200

    added comments

--- Milo/Program.cs
+++ Milo/Program.cs
@@ -17,19 +17,25 @@ namespace Milo
             try
             {
                 Console.WriteLine("Enter your first number");
+                // int.Parse converts the users input into a integer
                 a = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter your second number");
                 b = int.Parse(Console.ReadLine());
             }
+            // Catch Format Exception checks if the user puts in a non numeric value
             catch (FormatException)
             {
                 Console.WriteLine("ERROR : Input string was not in a correct format.");
                 
             }
+            // Passing the return value of menu into selection which is a int
             selection = Menu();
 
+            // Based off menu function return value we do a if statement to check if the value was between 0 - 4
             if (selection > 0 && selection < 5)
             {
+                // We put the selection value into the Calculate function by passing it and passing values a & b which were
+                // entered at the start, the return from Calculate is passed into ans and printed
                 ans = Calculate(selection, a, b);
                 Console.WriteLine(ans);
             }
@@ -39,6 +45,7 @@ namespace Milo
             };
 
 
+            // This is the menu function that takes the users input and returns it as a int
             int Menu()
             {
                 
@@ -47,6 +54,7 @@ namespace Milo
                 return  selectionOfMenu;
             }
 
+            // Calculate function that takes the input which was given at start does maths based on those numbers
             float Calculate(int choice, int x, int y)
             {
                 float result;
@@ -70,6 +78,8 @@ namespace Milo
                     if ( y == 0)
                     {
                         Console.WriteLine("You cannot divide by 0");
+
+                        // Exit if the user uses 0 as a number to divide
                         System.Environment.Exit(1);
                     }
                     result = (float)x/ (float)y;

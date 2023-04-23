5392d4e319cd3f25f95d45982a14b4fab6eee13d
Milo <milek1093@gmail.com>
2023-04-23 20:03:22 +0200

    all done luv

--- Excerice 2/Program.cs
+++ Excerice 2/Program.cs
@@ -11,10 +11,13 @@ namespace Excerice_2
         static void Main(string[] args)
         {
             string url;
+            // This variable will be used to check if the given url has http:// 
             string check = "htpps://";
             Console.WriteLine("Enter a url to see contents:");
             url = Console.ReadLine();
-
+            
+            // The compare function checks if the entered url starts with http:// by using the Substring function
+            // to get the first 8 parts of the entered string if they dont match it adds https:// to it
             if (string.Compare(check, url.Substring(0, 8)) != 0)
             {
                 url = "https://" + url;
